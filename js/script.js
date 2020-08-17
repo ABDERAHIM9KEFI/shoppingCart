16ee7aa88213963e1c218a846d40e97846eb4e25
ABDERAHIM9KEFI <67905222+ABDERAHIM9KEFI@users.noreply.github.com>
2020-08-17 22:24:36 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const productTable = document.querySelector('tbody')
-const removeCartItemButtons = document.getElementsByClassName('bi-x-square-fill')
+const removeCartItemButtons = document.getElementsByClassName('far fa-trash-alt')
 const plusbutton = document.getElementsByClassName('bi bi-plus')
 
 const minusbutton = document.getElementsByClassName('bi bi-dash')
 const heartbuttons = document.querySelectorAll('.fa-heart')
-const heartbuttons1 = document.querySelectorAll('#table-images')
+
 
 
 const prodNum = document.getElementById('prodNum')
@@ -40,10 +40,7 @@ function addToCartClicked(event) {
     prodNum.innerHTML = productNumber
     let newProduct = `
       <tr>
-      <th scope="row"><svg id="minusbutton" width="1em" height="1em" viewBox="0 0 16 16" class="bi white bi-x-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm9.854 4.854a.5.5 0 0 0-.708-.708L8 7.293 4.854 4.146a.5.5 0 1 0-.708.708L7.293 8l-3.147 3.146a.5.5 0 0 0 .708.708L8 8.707l3.146 3.147a.5.5 0 0 0 .708-.708L8.707 8l3.147-3.146z"/>
-    </svg>
-    <img id="table-images" src=${productImg}><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <th scope="row"><i class="far fa-trash-alt white"></i><img id="table-images" src=${productImg}><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
       </svg></th>
       <td>${productObj.productName}</td>
@@ -59,40 +56,48 @@ function addToCartClicked(event) {
       <td id="td-total">$${productObj.productPrice}</td>
     </tr>
     `
+    var sumtotal = document.querySelector('#pricetotal')
+    sumtotal.innerHTML= parseInt(sumtotal.innerHTML) + productObj.productPrice +"$"
     productTable.innerHTML += newProduct
-    var total1 = document.querySelectorAll('#td-total')
     for (let button of removeCartItemButtons) {
-      button.addEventListener('click', removeCartItem)
+    button.addEventListener('click', removeCartItem)
   }
-  function removeCartItem(event) {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.parentElement.remove()
-    productNumber--
-    prodNum.innerHTML = productNumber
-  }
-  heartbuttons1.forEach((el) =>  el.addEventListener("click",()=> el.classList.toggle('red')))
-  Array.from(removeCartItemButtons ).forEach((el) => el.addEventListener("mouseover",()=> el.classList.replace('white', 'red')))
-  Array.from(removeCartItemButtons ).forEach((el) => el.addEventListener("mouseout",()=> el.classList.replace('red', 'white'))) 
+  Array.from(removeCartItemButtons).forEach((el) => el.addEventListener("mouseover",()=> el.classList.replace('white', 'red')))
+  Array.from(removeCartItemButtons).forEach((el) => el.addEventListener("mouseout",()=> el.classList.replace('red', 'white'))) 
+  // plus buttons
   Array.from(plusbutton).map((el) => {
     el.addEventListener("click",()=>{
       el.nextElementSibling.innerHTML = parseInt(el.nextElementSibling.innerHTML) +1
-      var price= el.parentElement.nextElementSibling.nextElementSibling.innerText.replace("$","")
-      
-        total.innerHTML= parseInt(total.replace("$","")) + parseInt(price) + "$"
-      
-      
+      var price= el.parentElement.nextElementSibling
+      let total = el.parentElement.nextElementSibling.nextElementSibling
+      total.innerHTML = parseInt(total.innerHTML.replace('$', '')) + parseInt(price.innerHTML.replace('$', ''))+'$'
+      sumtotal.innerHTML=parseInt(sumtotal.innerHTML)+Number(price.innerHTML.replace('$',''))+'$'
     });
   });
+    // minus buttons
   Array.from(minusbutton).map((el) => {
-    el.addEventListener("click",()=>{
+    el.addEventListener("click",()=> {
       if(el.previousElementSibling.innerHTML>1){
         el.previousElementSibling.innerHTML = parseInt(el.previousElementSibling.innerHTML) -1
+        el.nextElementSibling.innerHTML = parseInt(el.nextElementSibling.innerHTML) +1
+      var price= el.parentElement.nextElementSibling
+      let total = el.parentElement.nextElementSibling.nextElementSibling
+      total.innerHTML = parseInt(total.innerHTML.replace('$', '')) - parseInt(price.innerHTML.replace('$', ''))+'$'
+      sumtotal.innerHTML=parseInt(sumtotal.innerHTML)-Number(price.innerHTML.replace('$',''))+'$'
       }
     });
   });
-  total1.forEach(total =>{
-    console.log(parseInt(total.innerHTML.replace("$",""))+1)
-        })
-}}
+  function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    var totalremoved = parseInt(buttonClicked.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML.replace('$',''))
+    sumtotal.innerHTML=sumtotal.innerHTML.replace('$','')- totalremoved + "$"
+    productNumber--
+    prodNum.innerHTML = productNumber 
+  }
+  const heartbuttons1 = document.querySelectorAll('.bi-heart')
+  heartbuttons1.forEach((el) =>  el.addEventListener("click",()=> el.classList.toggle('red')))
+  }
+}
 Array.from(heartbuttons).forEach((el) => el.addEventListener("click",()=> el.classList.toggle('is-active')))
-// <input id="quantity"  value="1" id="fname" name="fname">
+
